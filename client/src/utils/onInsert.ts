dd65f74d6c1558931188f056aac7a1ddf211e732
Thomas Card <tcardlab@gmail.com>
2023-09-19 00:27:40 -0400

    Demo: good typing

--- client/src/utils/onInsert.ts
+++ client/src/utils/onInsert.ts
@@ -2,6 +2,7 @@ import { ReducerEvent } from "@clockworklabs/spacetimedb-sdk";
 
 // Would be nice if SpacetimeDB exposed such types for building helper functions
 interface Table {
+  new (...args: any[]): any;
   onInsert: (cb: (value: any, reducerEvent: ReducerEvent | undefined) => void) => void;
   onDelete: (cb: (value: any, reducerEvent: ReducerEvent | undefined) => void) => void;
   onUpdate: (cb: (value: any, oldValue: any, reducerEvent: ReducerEvent | undefined) => void) => void;
@@ -13,7 +14,7 @@ interface Table {
 type Red = ReducerEvent | undefined
 export function onInsert<T extends Table> (table: T) {
   return (
-    cb: (v:T, red: Red)=>void,
+    cb: (v:InstanceType<T>, red: Red)=>void,
   ) => {
     table.onInsert(cb)
   }
